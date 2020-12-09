89d40cabc9ce74b8f40a26c935b2993ac9c0dc3f
Cédric Anne <cedric.anne@gmail.com>
2020-12-09 12:18:58 +0100

    Fix global criteria and some dropdown width

--- inc/search.class.php
+++ inc/search.class.php
@@ -2542,7 +2542,6 @@ JAVASCRIPT;
       $field_id = Html::cleanId("dropdown_criteria{$prefix}[$num][itemtype]$rand");
       $spanid   = Html::cleanId("show_".$request["itemtype"]."_".$prefix.$num."_$rand");
       // Ajax script for display search met& item
-      echo "<blockquote>";
 
       $params = [
          'action'          => 'display_criteria',
@@ -2562,14 +2561,13 @@ JAVASCRIPT;
          $params
       );
 
-      echo "<span id='$spanid'>";
+      echo "<div class='col-auto row' id='$spanid'>";
       if (isset($metacriteria['itemtype'])
           && !empty($metacriteria['itemtype'])) {
          $params['itemtype'] = $metacriteria['itemtype'];
          self::displayCriteria($params);
       }
-      echo "</span>";
-      echo "</blockquote>";
+      echo "</div>";
    }
 
    /**
